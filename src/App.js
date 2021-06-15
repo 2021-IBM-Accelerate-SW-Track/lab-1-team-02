cdf23df66262db443f5900d84c11ce32a128d3b7
ja0911 <ja0911@gmail.com>
2021-06-15 16:00:10 -0700

    added list component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Header from "./component/header"
+import Header from "./component/header/header"
+import List from "./component/list/list"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <List/>
     </div>
   );
 }
---
+++ src/component/index.js
@@ -0,0 +1,5 @@
+import Header from "/header/header"
+import List from "/list/list"
+
+export default Header 
+export default List
