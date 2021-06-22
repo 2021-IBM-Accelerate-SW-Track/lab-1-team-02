22df4e31549d72b3373a8f0a026f809d1e9eeb90
Vonesha Frost <voneshafrost@gmail.com>
2021-06-21 22:11:10 -0500

    VF branch with add, delete, and edit

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,3 +1,13 @@
+import { AppBar, Toolbar, Typography} from "@material-ui/core"
+
 export default function Header(){
-    return <div>2021 IBM Accelerate Software Track Lab Boiler Plate</div>
+    return (
+    <AppBar color="secondary" position="static">
+        <Toolbar>   
+            <Typography variant="h6">
+                2021 IBM Accelerate To-Do List
+            </Typography>
+        </Toolbar>
+    </AppBar>
+    )
 }
